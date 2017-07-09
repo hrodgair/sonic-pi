28ec2efc7e809f60134c2e42cf9d8d46ed05ebdb
roger <roger@MacRo.local>
2017-07-09 22:07:03 +0200

    Added degree-to-rate functions

--- src/sampling.rb
+++ src/sampling.rb
@@ -1,5 +1,19 @@
 # given a degree (f), return the sample speed rate
 # useful for pitching up and down samples
 define :degree2rate do |n|
-  2 ** (n/12)
+  2 ** (n.to_f/12)
+end
+
+define :degrees2rates do |degrees|
+  rates = []
+  degrees.each { |x| rates.push(degree2rate(x)) }
+  rates.ring
+end
+
+define :sample_degrees do
+  degrees = []
+  12.times do |i|
+    degrees.push(2 ** (i.to_f/12))
+  end
+  degrees.ring
 end
