ee40226b9327fc513ecbb4f83fb3877da51af50e
roger <roger@MacRo.local>
2017-07-09 22:13:06 +0200

    Added documentation (comments)

--- src/sampling.rb
+++ src/sampling.rb
@@ -1,15 +1,17 @@
 # given a degree (f), return the sample speed rate
-# useful for pitching up and down samples
+# useful for pitching up and down samples diatonically
 define :degree2rate do |n|
   2 ** (n.to_f/12)
 end
 
+# convert a list of degrees to sample rates
 define :degrees2rates do |degrees|
   rates = []
   degrees.each { |x| rates.push(degree2rate(x)) }
   rates.ring
 end
 
+# returns a list of rates that represent the 12 notes in a scale
 define :sample_degrees do
   degrees = []
   12.times do |i|
